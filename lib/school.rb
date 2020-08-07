478654601bc465fdc464bbc8208d1b3dd09d2e35
Alan Coomes <acoomes1@cord.edu>
2020-08-07 23:41:42 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,13 @@ class School
 
   
   def add_student(student_name, grade)
-    
+      @student_name = student_name
+    @grade = grade
+    if !ROSTER.include?(grade)
+      ROSTER[grade] = []
+      ROSTER[grade] << "#{student_name}"
+    else
+      ROSTER[grade] << "#{student_name}"
 end
   
   def grade(grade)
@@ -18,3 +24,4 @@ end
   def sort 
   end
 end 
+
