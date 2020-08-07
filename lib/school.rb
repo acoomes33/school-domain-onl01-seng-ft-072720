e1f47016df6b4192c360a7b1fb43b3aa4ab9db4b
Alan Coomes <acoomes1@cord.edu>
2020-08-07 23:53:40 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,36 +1,18 @@
-# code here!
 class School
-  attr_accessor :roster, :student_name
-
+  attr_accessor :roster, :student_name, :school_name
+  
   def initialize(school_name)
     @school_name = school_name
-    @roster = []
+    @roster = {}
   end
 
-  
   def add_student(student_name, grade)
-  if @roster[grade]
-    @roster[grade] << student_name
-  else
-    @roster[grade] = []
-    @roster[grade] << student_name
+    if @roster[grade] == true
+      @roster[grade] << student_name
+    else
+      @roster[grade] = []
+      @roster[grade] << student_name
+    end
   end
-end
-  
-  def grade(grade)
-     roster.detect do |a, b| 
-      if a == grade        
-        return b        
-        end 
-    end 
-end
-  
-  def sort 	
-    new_hash = {}
-   @roster.each do |a, b| 
-     new_hash[a] = b.sort 
-   end 
-  new_hash
- end 
 
 end
