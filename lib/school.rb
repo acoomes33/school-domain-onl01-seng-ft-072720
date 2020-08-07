545fabfceeeacb0f95d66a9260d50a361e9c484b
Alan Coomes <acoomes1@cord.edu>
2020-08-07 23:38:25 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
 # code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = []
+  end
+
+  
+  def add_student(student_name, grade)
+    
+end
+  
+  def grade(grade)
+end
+  
+  def sort 
+  end
+end 
